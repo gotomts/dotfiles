49030267fda16c01a2636e8d34680355dbaabbca
gotomts <mh.goto.web@gmail.com>
2024-02-08 12:52:37 +0900

    add python

---
+++ setup/install/08_python.zsh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+source ${HOME}/.dotfiles/setup/util.zsh
+
+installs=(
+  poetry=1.2.0
+)
+
+asdf plugin-add python
+asdf install python latest
+asdf install python 3.12.1
+asdf global python 3.12.1
+
+for install ${installs[@]} do
+  pipx install ${install}
+done
